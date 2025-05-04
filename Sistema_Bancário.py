0b781136c069986212355099a11678aca71f1adb
Otavio2007 <otavioaredes62@gmail.com>
2025-05-04 14:56:43 -0300

    Update Sistema_Bancário.py
    Better version from my bank with python

--- Sistema_Bancário.py
+++ Sistema_Bancário.py
@@ -1,50 +1,81 @@
-import time
+from datetime import datetime
 
-opcao = 0
-dinheiro = 0
+SENHA_CORRETA = "python123"
 
-while opcao!=4:
-        opcao=int(input('\nO que deseja fazer?'
-                    '\n1-Fazer depósito'
-                    '\n2-Sacar dinheiro'
-                    '\n3-Ver o saldo'
-                    '\n4-Encerrar operação -----> '))
-        if opcao==1:
-           senha = input('\nInsira sua senha: ')
-           print('Processando...')
-           time.sleep(3)
-           if senha == "python123":
-             ad=float(input('\nInsira valor: '))
-             dinheiro=dinheiro+ad
+# Função que inicia o sistema bancário
+def sistema_bancario():
+    opcao = 0
+    dinheiro = 0
+    extrato = []
+    transacoes_hoje = 0
+    data_atual = datetime.now().date()
 
-             print('Valor inserido ;)')
-           else:
-            print('\nSenha incorreta :(')
+    while opcao != 4:
+        agora = datetime.now()
+        data_hoje = agora.date()
 
-        elif opcao==2:
-            senha = input('\nInsira sua senha: ')
-            print('Processando...')
-            time.sleep(3)
-            if senha == "python123":
-              ret=float(input('\nValor a retirar: '))
-              if dinheiro<ret:
-               print('Impossível sacar esse valor, verifique seu saldo na opção 3')
-              else:
-               dinheiro=dinheiro-ret
+        # Se mudou o dia, resetamos o contador
+        if data_hoje != data_atual:
+            transacoes_hoje = 0
+            data_atual = data_hoje
+
+        print(f'\nTransações realizadas hoje: {transacoes_hoje}/10')
+        opcao = int(input('\nO que deseja fazer?'
+                          '\n1 - Fazer depósito'
+                          '\n2 - Sacar dinheiro'
+                          '\n3 - Ver extrato'
+                          '\n4 - Encerrar operação -----> '))
+
+        if opcao in [1, 2] and transacoes_hoje >= 10:
+            print("Você excedeu o número de transações permitidas para hoje. Tente novamente amanhã.")
+            continue
+
+        if opcao == 1:
+            ad = float(input('\nInsira o valor do depósito: '))
+            if ad > 0:
+                dinheiro += ad
+                horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                extrato.append(f'[{horario}] Depósito: +R$ {ad:.2f}')
+                transacoes_hoje += 1
+                print('Depósito realizado com sucesso ;)')
             else:
-             print('Senha incorreta :(')
+                print('Valor inválido! O depósito deve ser positivo.')
 
-        elif opcao==3:
-            senha = input('\nInsira sua senha: ')
-            print('Processando...')
-            time.sleep(3)
-            if senha == "python123":
-              print(f'\nSeu saldo é {dinheiro}')
+        elif opcao == 2:
+            ret = float(input('\nValor a sacar: '))
+            if ret > dinheiro:
+                print('Saldo insuficiente para realizar o saque.')
+            elif ret <= 0:
+                print('Valor inválido! O saque deve ser positivo.')
             else:
-                print('Senha incorreta :(')
+                dinheiro -= ret
+                horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                extrato.append(f'[{horario}] Saque: -R$ {ret:.2f}')
+                transacoes_hoje += 1
+                print('Saque realizado com sucesso!')
+
+        elif opcao == 3:
+            print('\n--- Extrato ---')
+            if not extrato:
+                print('Nenhuma movimentação registrada.')
+            else:
+                for movimento in extrato:
+                    print(movimento)
+            print(f'\nSaldo atual: R$ {dinheiro:.2f}')
 
         elif opcao == 4:
-          print('\nTenha um ótimo dia :)')
+            print('\nTenha um ótimo dia :)')
 
         else:
-          print('\nOpção inválida, selecione as opções presentes na tela')
+            print('\nOpção inválida, selecione as opções presentes na tela')
+
+# Função para autenticação inicial
+def autenticar():
+    senha_inicial = input("Insira a senha para iniciar o sistema bancário: ")
+    if senha_inicial == SENHA_CORRETA:
+        sistema_bancario()
+    else:
+        print("Senha incorreta. Acesso negado.")
+
+# Inicia o programa
+autenticar()
